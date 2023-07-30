21fd08d8eae1e5d2d10a5e33f48b56fd5dded105
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-07-30 23:22:06 +0530

    Update Calc.java

--- src/main/java/com/db/demo/math/Calc.java
+++ src/main/java/com/db/demo/math/Calc.java
@@ -10,8 +10,7 @@ public class Calc {
 		return i - j;
 	}
 
-	public int divNums(int i, int j) {
-		return i / j;
-	}
-
 }
+
+
+//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
